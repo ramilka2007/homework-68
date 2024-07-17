865bd76d9cfc7be535d0ef01a59749c0684d6bc4
ramilka <lilya1976.03.03@gmail.com>
2024-07-17 14:53:54 +0600

    Сделал удаление задания на кнопку delete; сделал так, чтобы чекбокс можно было поменять на сделанное и несделанное задание.

--- src/components/ToDoListItem/ToDoListItem.tsx
+++ src/components/ToDoListItem/ToDoListItem.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { Task } from '../../types';
+import { useDispatch } from 'react-redux';
+import {
+    DeleteTask,
+    EditTask,
+    fetchToDoList,
+} from '../../containers/ToDoList/toDoListSlice';
 
 interface Props {
     task: Task;
 }
 
 const ToDoListItem: React.FC<Props> = ({ task }) => {
+    const dispatch = useDispatch();
+
+    const changeCheckBox = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const updateTask = { ...task };
+        updateTask.done = event.target.checked;
+        await dispatch(EditTask(updateTask));
+        await dispatch(fetchToDoList());
+    };
+
+    const deleteTask = async () => {
+        await dispatch(DeleteTask(task.id));
+        await dispatch(fetchToDoList());
+    };
     return (
         <div className="task card w-50 mx-auto mb-4">
             <div className="card-body d-flex align-items-center justify-content-between">
@@ -17,10 +36,12 @@ const ToDoListItem: React.FC<Props> = ({ task }) => {
                     <input
                         className="form-check-input"
                         type="checkbox"
+                        onChange={changeCheckBox}
+                        checked={task.done}
                     />
                 </div>
                 <div className="col-4">
-                    <button className="ms-4 btn btn-danger">
+                    <button className="ms-4 btn btn-danger" onClick={deleteTask}>
                         Delete
                     </button>
                 </div>
--- src/containers/ToDoList/toDoListSlice.ts
+++ src/containers/ToDoList/toDoListSlice.ts
@@ -19,36 +19,51 @@ export const fetchToDoList = createAsyncThunk('toDoList/fetch', async () => {
   return list || null;
 });
 
+export const EditTask = createAsyncThunk(
+    'toDoList/edit',
+    async (task: Task) => {
+      const updated = { task: task.task, done: task.done };
+      await axiosApi.put(`toDoList/${task.id}.json`, updated);
+    },
+);
+
+export const DeleteTask = createAsyncThunk(
+    'toDoList/delete',
+    async (task: Task) => {
+      await axiosApi.delete(`toDoList/${task}.json`);
+    },
+);
+
 export const ToDoListSlice = createSlice({
   name: 'toDoList',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchToDoList.pending, (state: toDoListState) => {
-        state.isLoading = true;
-        state.error = false;
-      })
-      .addCase(fetchToDoList.fulfilled, (state: toDoListState, action) => {
-        const allTasks: Task[] = [];
+        .addCase(fetchToDoList.pending, (state: toDoListState) => {
+          state.isLoading = true;
+          state.error = false;
+        })
+        .addCase(fetchToDoList.fulfilled, (state: toDoListState, action) => {
+          const allTasks: Task[] = [];
 
-        if (action.payload) {
-          for (const [key, value] of Object.entries(action.payload)) {
-            allTasks.push({
-              id: key,
-              task: value.task,
-              done: value.done,
-            });
+          if (action.payload) {
+            for (const [key, value] of Object.entries(action.payload)) {
+              allTasks.push({
+                id: key,
+                task: value.task,
+                done: value.done,
+              });
+            }
           }
-        }
 
-        state.tasks = allTasks;
-        state.isLoading = false;
-      })
-      .addCase(fetchToDoList.rejected, (state: toDoListState) => {
-        state.isLoading = false;
-        state.error = true;
-      });
+          state.tasks = allTasks;
+          state.isLoading = false;
+        })
+        .addCase(fetchToDoList.rejected, (state: toDoListState) => {
+          state.isLoading = false;
+          state.error = true;
+        });
   },
 });
 
